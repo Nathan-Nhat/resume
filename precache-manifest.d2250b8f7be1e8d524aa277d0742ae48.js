b0bda54e0b6e1168116048348383d69cce465c0d
ttnhat <trantrungnhat6196@gmail.com>
2020-06-06 18:25:38 +0700

    Updates

---
+++ precache-manifest.d2250b8f7be1e8d524aa277d0742ae48.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "477d90d25bbd26bf91c1cdf537168504",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "a204552fcccb6525c604",
+    "url": "/resume/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "9e12e47cb49112fdbb0e",
+    "url": "/resume/static/css/main.de2605d4.chunk.css"
+  },
+  {
+    "revision": "a204552fcccb6525c604",
+    "url": "/resume/static/js/2.5274ee42.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/resume/static/js/2.5274ee42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e12e47cb49112fdbb0e",
+    "url": "/resume/static/js/main.37c4e621.chunk.js"
+  },
+  {
+    "revision": "bcaa68c0b5f7adc089f7",
+    "url": "/resume/static/js/runtime-main.05fbbc33.js"
+  },
+  {
+    "revision": "6d4dd6a7f2c9f6ceb90600bddfeb137a",
+    "url": "/resume/static/media/app1.6d4dd6a7.png"
+  },
+  {
+    "revision": "ec1e8477b29554aa84568a322d410203",
+    "url": "/resume/static/media/avatar.ec1e8477.jpeg"
+  },
+  {
+    "revision": "6306fda246ff25a97204f17a558bc541",
+    "url": "/resume/static/media/background.6306fda2.jpeg"
+  },
+  {
+    "revision": "2028c0ec7459ebcc8c2702bca15fe6d0",
+    "url": "/resume/static/media/blog1.2028c0ec.png"
+  },
+  {
+    "revision": "93d7867dac77827ae4592d7e70867448",
+    "url": "/resume/static/media/blog2.93d7867d.png"
+  },
+  {
+    "revision": "f686ba35dc2ffc169e345f81ba813a72",
+    "url": "/resume/static/media/blog3.f686ba35.png"
+  },
+  {
+    "revision": "fd253ae25ae8c19deeab12c640f2cfc1",
+    "url": "/resume/static/media/expertise.fd253ae2.png"
+  },
+  {
+    "revision": "7428c0e4a2d2b5c1314d3f98f7ccbcbb",
+    "url": "/resume/static/media/fbnewpng.7428c0e4.png"
+  },
+  {
+    "revision": "c077f1d55fac5d71dd7c8e31ba8db1ac",
+    "url": "/resume/static/media/linkedin.c077f1d5.png"
+  },
+  {
+    "revision": "8c77dbee60fc56c7832f2bc68d2a377b",
+    "url": "/resume/static/media/passion1.8c77dbee.jpg"
+  },
+  {
+    "revision": "55e7f665c86dfafcbcfe4f22d61fb6d9",
+    "url": "/resume/static/media/passion2.55e7f665.jpg"
+  },
+  {
+    "revision": "f70016fcfde61bad15fade7a2965f876",
+    "url": "/resume/static/media/passion3.f70016fc.jpg"
+  },
+  {
+    "revision": "8e0dd39479a3462a7f55d987ef0f0d9b",
+    "url": "/resume/static/media/passions.8e0dd394.png"
+  },
+  {
+    "revision": "1356f85043a0ff8540fed51bf7382643",
+    "url": "/resume/static/media/projects.1356f850.png"
+  },
+  {
+    "revision": "08182fea116b4cc33fe0f7eddb05e01e",
+    "url": "/resume/static/media/services.08182fea.png"
+  },
+  {
+    "revision": "b642a66623af2a161b1e415ffee95bd9",
+    "url": "/resume/static/media/web1.b642a666.png"
+  },
+  {
+    "revision": "0440fd50c61146eef9d97334b87f4dcd",
+    "url": "/resume/static/media/web2.0440fd50.png"
+  }
+]);
